d079bdcb473e65b17b8bc05429e33b977f5039b3
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2021-01-21 11:22:04 +0200

    v 1.01
    add folder with copmonents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Car repair',
+        day: 'Jan 21th at 18:00pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Learn React Hooks',
+        day: 'Jan 21th from 19:00 to 22:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Jan 22th after 17:30pm',
+        reminder: false,
+    }
+])
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div className="container">
+    <Header />
+    <Tasks  tasks={tasks}/>
     </div>
   );
 }
