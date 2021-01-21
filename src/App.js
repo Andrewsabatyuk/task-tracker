4f47587f2af4b5d0b57d8cce64f455d48169cd4e
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2021-01-21 15:35:39 +0200

    v 1.03
    toggle reminder and delete icon

--- src/App.js
+++ src/App.js
@@ -24,10 +24,23 @@ function App() {
         reminder: false,
     }
 ])
+
+
+//toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+//delete task
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
+
+
   return (
     <div className="container">
     <Header />
-    <Tasks  tasks={tasks}/>
+    { tasks.length > 0 ? <Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
