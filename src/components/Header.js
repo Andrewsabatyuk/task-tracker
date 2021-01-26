3b0f51bb0ebee6e4d6c2bc415206387299e225ba
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2021-01-26 15:37:02 +0200

    UI done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Button from './Button'
 
-const Header = () => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ( {onAdd, showAdd} ) => {
+	// const onClick = () => {
+	//     console.log('click')
+	// }
 
-    return (
-        <header className='header'>
-            <h1>Task Traker</h1>
-            <Button color='green' text='Add'  onClick={onClick} />
-        </header>
-    )
+	return (
+		<header className='header'>
+			<h1>Task Traker</h1>
+      <Button color={showAdd ? 'red' : 'green'}
+      text={showAdd ? 'Close' : 'Add'}  onClick={onAdd} />
+		</header>
+	)
 }
 
 export default Header
